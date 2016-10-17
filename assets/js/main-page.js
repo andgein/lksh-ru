36e7e6c8dce0515a80b87c79387796e4675be7d2
Andrew Gein <andgein@yandex.ru>
2016-10-17 13:52:51 +0500

    Add parallax for portratis and some fixes

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
+	var initParallax = function($elements) {
+		$elements.each(function() {
+			var $self = $(this);
+			// $self.data('init-left', $self.css('left'));
+			$self.data('init-top', $self.css('top'));
 
+			for (var level = 1; level <= 4; level++)
+				if ($self.hasClass('level-' + level))
+					$self.data('level', level)
+		});
+	}
+
+	var createPortraitsParallax = function() {
+		var $container = $('.portraits')
+		var $portraits = $container.find('.portrait');
+		initParallax($portraits);
+
+		var containerTop = $container.offset().top;
+		var containerBottom = containerTop + $container.height();
+		$(window).scroll(function () {
+			var scrollTop = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scrollTop >= containerTop - windowHeight && scrollTop <= containerBottom) {
+				var diff = scrollTop - containerTop;
+				$portraits.each(function() {
+					var $self = $(this);
+					var coeff = (5 - $self.data('level')) * 0.05;
+					var top = Math.floor(parseInt($self.data('init-top')) - coeff * diff);
+					$self.css('top', top + 'px');
+				});
+			}
+		});
+	}
+
+	createPortraitsParallax();
 });

a83725bb44c4d5dc238864bb6421483f3d6e717f
Andrew Gein <andgein@yandex.ru>
2016-10-30 17:53:29 +0300

    Link to the information about how to get Viewport for enrolled list

--- assets/js/enrolled.js
+++ assets/js/enrolled.js
@@ -9,9 +9,12 @@ $(document).ready(function(){
 		$('.enrolled-list-switcher').removeClass('active');
 		$self.addClass('active');
 
+		/* Hide footer for disable fast appearance of it */
+		$('footer').hide();
 		$enrolledList.find('.by-name, .by-parallel').fadeOut(100);
 		setTimeout(function () {
 			$enrolledList.find($self.attr('href')).fadeIn('fast');
+			$('footer').show();
 		}, 100);
 	})
 });
